84a01e9e61888b6c2b7b41b17fd8b71c96bda118
alancavalcante-dev <ala.pereiradocavalcante@gmail.com>
2024-10-30 07:32:28 -0300

    Corre

--- gestao_estoque/models.py
+++ gestao_estoque/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class TipoProduto(models.Model):
     type = models.CharField(max_length=50, unique=True)
+    image = models.ImageField(upload_to='tipos/', null=True, blank=True)
 
     def __str__(self):
         return self.type
